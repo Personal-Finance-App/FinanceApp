a4c34a2bc56bfcc8823cb0d7a55fecf027b37c34
XeonStaf <alexey.astafiev_external@wartsila.com>
2022-07-07 22:52:58 +0300

    Create Some Response Entity from Tinkoff responses

---
+++ src/main/java/financeapp/BankConnection/Api/Tinkoff/ResponseEntitys/Session.java
@@ -0,0 +1,46 @@
+
+package financeapp.BankConnection.Api.Tinkoff.ResponseEntitys;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import javax.annotation.Generated;
+
+@Generated("jsonschema2pojo")
+public class Session {
+
+    @SerializedName("resultCode")
+    @Expose
+    private String resultCode;
+    @SerializedName("payload")
+    @Expose
+    private Payload payload;
+    @SerializedName("trackingId")
+    @Expose
+    private String trackingId;
+
+    public String getResultCode() {
+        return resultCode;
+    }
+
+    public void setResultCode(String resultCode) {
+        this.resultCode = resultCode;
+    }
+
+    public Payload getPayload() {
+        return payload;
+    }
+
+    public void setPayload(Payload payload) {
+        this.payload = payload;
+    }
+
+    public String getTrackingId() {
+        return trackingId;
+    }
+
+    public void setTrackingId(String trackingId) {
+        this.trackingId = trackingId;
+    }
+
+}
