b03c14a838bc70af4612bf46559861a24077fc71
XeonStaf <alexey.astafiev_external@wartsila.com>
2022-08-07 22:25:43 +0700

    reports list for user

--- src/main/java/financeapp/monthReport/repos/ReportRepo.java
+++ src/main/java/financeapp/monthReport/repos/ReportRepo.java
@@ -4,6 +4,10 @@ import financeapp.monthReport.entity.Report;
 import financeapp.users.CustomUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReportRepo extends JpaRepository<Report, Long> {
     Report findReportByLinkedUserAndMonthAndYear(CustomUser user, Integer month, Integer year);
+
+    List<Report> findReportByLinkedUser(CustomUser user);
 }
