fb04245befea9af87b262ec543f234c67e81b0ae
XeonStaf <alexey.astafiev_external@wartsila.com>
2022-08-13 18:02:32 +0700

    More Secure!

--- src/main/java/financeapp/config/SecurityConfiguration.java
+++ src/main/java/financeapp/config/SecurityConfiguration.java
@@ -48,7 +48,7 @@ public class SecurityConfiguration {
                 .antMatchers("/accounts").fullyAuthenticated()
                 .antMatchers("/tinkof/*").fullyAuthenticated()
                 .antMatchers("/reports").fullyAuthenticated()
-                .antMatchers("/report/*").fullyAuthenticated()
+                .antMatchers("/report/**").fullyAuthenticated()
                 .antMatchers("/monthTransaction/*").fullyAuthenticated()
                 .and().formLogin();
         return http.build();
