c3db11dc56bf8aa4d8e612543170925c403d0885
d <d>
2022-08-23 13:46:39 +0300

    feat: rename field externalAccountNumber on AccountNumber

--- src/main/java/financeapp/accounts/AccountData.java
+++ src/main/java/financeapp/accounts/AccountData.java
@@ -8,6 +8,6 @@ import lombok.Data;
 public class AccountData {
     private String name;
     private String id;
-    private String externalAccountNumber;
+    private String AccountNumber;
     private String type;
 }
