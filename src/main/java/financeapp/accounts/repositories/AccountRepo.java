a72829d34bf78ac06c9f7673c628e201a807d511
d <d>
2022-08-27 17:03:00 +0300

    feat: delete unnecessary import

--- src/main/java/financeapp/accounts/repositories/AccountRepo.java
+++ src/main/java/financeapp/accounts/repositories/AccountRepo.java
@@ -3,6 +3,7 @@ package financeapp.accounts.repositories;
 import financeapp.accounts.models.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface AccountRepo extends JpaRepository<Account, UUID> {
