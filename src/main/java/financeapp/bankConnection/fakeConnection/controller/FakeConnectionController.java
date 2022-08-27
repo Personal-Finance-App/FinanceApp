6a5bb2fe050ec381f62de62f2d71d43188e11ecd
d <d>
2022-08-27 17:02:03 +0300

    feat: add some methods

--- src/main/java/financeapp/bankConnection/fakeConnection/controller/FakeConnectionController.java
+++ src/main/java/financeapp/bankConnection/fakeConnection/controller/FakeConnectionController.java
@@ -1,9 +1,12 @@
 package financeapp.bankConnection.fakeConnection.controller;
 
 import financeapp.accounts.models.Account;
+import financeapp.accounts.services.AccountService;
 import financeapp.bankConnection.fakeConnection.service.FakeConnectionService;
 import financeapp.transaction.models.AbstractTransaction;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,23 +16,30 @@ import java.util.Collection;
 @RestController
 public class FakeConnectionController {
     private final FakeConnectionService fakeConnectionService;
+    private final AccountService accountService;
 
-    public FakeConnectionController(FakeConnectionService fakeConnectionService) {
+    public FakeConnectionController(FakeConnectionService fakeConnectionService, AccountService accountService) {
         this.fakeConnectionService = fakeConnectionService;
+        this.accountService = accountService;
     }
     @PostMapping("/fake/addTransaction")
     public ResponseEntity<Collection<AbstractTransaction>> addTransaction(@RequestParam (value = "IdAccount") String id) {
-        return ResponseEntity.ok().body(fakeConnectionService.addTransactions(id));
+        return ResponseEntity.ok().body(fakeConnectionService.AddTransactions(id));
     }
 
     @PostMapping("/fake/createAccount")
-    public ResponseEntity<Account> createAccount(@RequestParam (value = "EmailUser") String email,
+    public ResponseEntity<Account> createAccount(Authentication authentication,
                                                  @RequestParam (value = "Type") String type){
-        return ResponseEntity.ok().body(fakeConnectionService.CreateAccount(type, email));
+        return ResponseEntity.ok().body(fakeConnectionService.CreateAccount(type, authentication.getName()));
     }
 
     @PostMapping("/fake/createAccounts")
-    public ResponseEntity<Collection<Account>> createAccounts(@RequestParam (value = "EmailUser") String email){
-        return  ResponseEntity.ok().body(fakeConnectionService.CreateAccounts(email));
+    public ResponseEntity<Collection<Account>> createAccounts(Authentication authentication){
+        return  ResponseEntity.ok().body(fakeConnectionService.CreateAccounts(authentication.getName()));
+    }
+
+    @GetMapping("/fake/getAll")
+    public ResponseEntity<Collection<Account>> getAll(){
+        return ResponseEntity.ok().body(accountService.getAll());
     }
 }
