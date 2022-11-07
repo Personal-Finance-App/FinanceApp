9beb2b60117dbac152b90e4d57e4d0d37313ce28
eugeniya.mo <eugeniya.mo@gmail.com>
2022-11-07 16:34:12 +0300

    feat: fixed defects

--- src/main/java/financeapp/bankConnection/tinkoff/api/responseEntitys/PinConfirm/SetUpPinAnswer.java
+++ src/main/java/financeapp/bankConnection/tinkoff/api/responseEntitys/PinConfirm/SetUpPinAnswer.java
@@ -3,7 +3,6 @@ package financeapp.bankConnection.tinkoff.api.responseEntitys.PinConfirm;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
-
 import javax.annotation.Generated;
 
 @Generated("jsonschema2pojo")
