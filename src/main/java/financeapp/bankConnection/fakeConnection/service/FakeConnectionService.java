ee9436317a6a1edaea08e5c665970a2b55f8531c
d <d>
2022-11-15 19:23:55 +0300

    feat: refactor FakeConnectionService.java

--- src/main/java/financeapp/bankConnection/fakeConnection/service/FakeConnectionService.java
+++ src/main/java/financeapp/bankConnection/fakeConnection/service/FakeConnectionService.java
@@ -1,7 +1,6 @@
 package financeapp.bankConnection.fakeConnection.service;
 
 import financeapp.accounts.models.Account;
-import financeapp.accounts.repositories.AccountRepo;
 import financeapp.accounts.services.AccountService;
 import financeapp.bankConnection.fakeConnection.abstractFactory.FakeAccountManager;
 import financeapp.bankConnection.fakeConnection.factoryMethod.FakeAccountFactory;
@@ -9,50 +8,51 @@ import financeapp.bankConnection.fakeConnection.factoryMethod.FakeCreditAccount;
 import financeapp.bankConnection.fakeConnection.factoryMethod.FakeDebitAccount;
 import financeapp.bankConnection.fakeConnection.factoryMethod.FakeSavingAccount;
 import financeapp.bankConnection.fakeConnection.tools.RandomUtility;
-import financeapp.transaction.TransactionRepo;
 import financeapp.transaction.models.AbstractTransaction;
 import financeapp.transaction.models.IncomeTransaction;
 import financeapp.transaction.models.PayTransaction;
 import financeapp.transaction.models.TransferTransaction;
+import financeapp.transaction.services.TransactionService;
 import financeapp.users.CustomUser;
-import financeapp.users.UserRepo;
-import org.springframework.beans.factory.annotation.Autowired;
+import financeapp.users.UserService;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.*;
 
 @Service
 public class FakeConnectionService {
     private final int countTransaction;
     private final RandomUtility randomizer;
-    private final AccountRepo accountRepo;
-    private final UserRepo userRepo;
+    private final UserService userService;
+    private final AccountService accountService;
     private FakeAccountFactory factory;
-    private final TransactionRepo transactionRepo;
+    private final TransactionService transactionService;
 
-    public FakeConnectionService(RandomUtility randomizer, AccountRepo accountRepo, UserRepo userRepo,
-                                 TransactionRepo transactionRepo) {
+    public FakeConnectionService(RandomUtility randomizer,
+                                 AccountService accountService, UserService userService,
+                                TransactionService transactionService) {
         this.randomizer = randomizer;
-        this.accountRepo = accountRepo;
-        this.userRepo = userRepo;
+        this.userService = userService;
+        this.accountService = accountService;
         this.countTransaction = 50;
-        this.transactionRepo = transactionRepo;
+        this.transactionService = transactionService;
     }
 
     public List<Account> CreateAccounts(String email){
-        CustomUser user = userRepo.findCustomUserByEmail(email);
+        CustomUser user = userService.findUserByEmail(email);
         FakeAccountManager factory = new FakeAccountManager();
         List<Account> accounts = new ArrayList<>();
         accounts.add(factory.getCreditAccount(user));
         accounts.add(factory.getSavingAccount(user));
         accounts.add(factory.getDebitAccount(user));
-        accountRepo.saveAll(accounts);
+        accountService.CreateAccountFromPayload(accounts, user);
         List<Account> accountList = addTransactionOnAccount(accounts);
         return accountList;
     }
 
     public Account CreateAccount(String type, String email){
-        CustomUser user = userRepo.findCustomUserByEmail(email);
+        CustomUser user = userService.findUserByEmail(email);
         if(type.equalsIgnoreCase("debit")){
             factory = new FakeDebitAccount();
             return saveAccount(user);
@@ -68,7 +68,7 @@ public class FakeConnectionService {
 
 
     public List<AbstractTransaction> AddTransactions(String id){
-        Account account = accountRepo.findAccountByIdInSystem(id);
+        Account account = accountService.getById(id);
         List<AbstractTransaction> abstractTransactions = new ArrayList<AbstractTransaction>();
         for(int i = countTransaction; i > 0; i--){
             AbstractTransaction abstractTransaction = choiceTypeTransaction();
@@ -77,10 +77,11 @@ public class FakeConnectionService {
             abstractTransaction.setDescription("test transaction");
             abstractTransaction.setMerchant(RandomUtility.getRandomNameForMerchant());
             abstractTransaction.setCategory(randomizer.getRandomCategory());
+//            abstractTransaction.setAccount(account);
             abstractTransactions.add(abstractTransaction);
         }
-        transactionRepo.saveAll(abstractTransactions);
-        account.setTransactionList(abstractTransactions);
+        transactionService.saveTransactions(abstractTransactions, account, LocalDateTime.now());
+//        account.setTransactionList(abstractTransactions);
         return abstractTransactions;
     }
 
@@ -101,7 +102,9 @@ public class FakeConnectionService {
     }
     public Account saveAccount(CustomUser user){
         Account account = factory.CreateAccount(user);
-        accountRepo.save(account);
+        List<Account> accounts = new ArrayList<>();
+        accounts.add(account);
+        accountService.CreateAccountFromPayload(accounts, user);
         return account;
     }
 }
