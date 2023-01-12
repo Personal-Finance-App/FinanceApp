779629859b05c6291d8703b1a86cae0dfd0be11e
Софья <orlova.sofia2002@gmail.com>
2023-01-12 14:51:44 +0300

    feat: add function

--- src/main/java/financeapp/monthReport/repos/ReportRepo.java
+++ src/main/java/financeapp/monthReport/repos/ReportRepo.java
@@ -9,6 +9,7 @@ import java.util.List;
 public interface ReportRepo extends JpaRepository<Report, Long> {
     Report findReportByLinkedUserAndMonthAndYear(CustomUser user, Integer month, Integer year);
 
+    Report findReportByIdAndLinkedUser(Long id, CustomUser user);
     List<Report> findReportByLinkedUser(CustomUser user);
     void deleteReportByLinkedUserAndMonthAndYear(CustomUser user, Integer month, Integer year);
     void deleteReportById(Long id);
